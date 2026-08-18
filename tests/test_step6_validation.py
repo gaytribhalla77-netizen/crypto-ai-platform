c859590632e579edd6a75548d96262e4efaa2a63
gaytribhalla77-netizen <gaytribhalla77@gmail.com>
2026-08-18 13:12:30 +0530

    Fix Step 6 certification test import

--- tests/test_step6_validation.py
+++ tests/test_step6_validation.py
@@ -1,4 +1,4 @@
-from certification.step6 import offline_step6_passes, run_offline_step6_gates
+from backend.certification.step6 import offline_step6_passes, run_offline_step6_gates
 
 
 def test_step6_offline_validation_gates_pass():
